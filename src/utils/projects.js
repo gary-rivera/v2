a225ce363bab015be86711b23b100c5e01321110
Gary Rivera <a.gary.rivera@gmail.com>
2021-08-10 17:09:21 -0700

    updated projects.js to include "productiv" app and updated files to use external links to gifs hosted, instead of local to repo

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -1,9 +1,7 @@
 import warblerImg from '../img/projectScreenshots/warblerImg.png'
-import warblerGif from '../img/projectScreenshots/warblerGif.gif'
 import joblyImg from '../img/projectScreenshots/joblyImg.png'
-import joblyGif from '../img/projectScreenshots/joblyGif.gif'
-import flappy from '../img/projectScreenshots/flappy.png'
-import flappyGif from '../img/projectScreenshots/flappyGif.gif'
+import flappyImg from '../img/projectScreenshots/flappy.png'
+import productivImg from '../img/projectScreenshots/productivImg.png';
 
 /**
  * project format: 
@@ -23,26 +21,35 @@ const projects = [
       deployedLink: 'http://warblology.herokuapp.com/',
       technologies: ['Python', 'Flask', 'PostgreSQL'],
       img: warblerImg,
-      gif: warblerGif,
-      description: 'A Twitter clone deployed on Heroku built using Python, Flask, PostgreSQL, and SQLAlchemy'
+      gif: 'https://media.giphy.com/media/eSsszxm0mGbk4reXnD/giphy.gif',
+      description: 'A Twitter clone deployed on Heroku built using Python, Flask, and PostgreSQL. This application handles database relational tables between users, followers, and "liked" posts via SQLAlchemy.'
     }, 
     {
       title: "Jobly",
       repoLink: 'https://github.com/jacobson-ben/jobly-total',
-      deployedLink: 'https://jobly-solutions.surge.sh/',
+      deployedLink: 'http://joelburton-jobly.surge.sh/',
       technologies: ['Javascript', 'React', 'Node.js', 'Express', 'PostgresSQL'],
       img: joblyImg,
-      gif: joblyGif,
-      description: 'An application replicating the job listings, company an user profiles, built with a React front-end hosted on Surge and Express/Node.js back-end hosted on Heroku'
+      gif: 'https://media.giphy.com/media/bWieBvTg3rD6bEkCXf/giphy.gif',
+      description: 'A mock job-searching application that features job filtering through companies, saves user data, such as jobs applied to, and even features company profiles profiles. Developed using React Hooks on the front-end, which is hosted on Surge and Express/Node.js back-end, hosted on Heroku.'
+    }, 
+    {
+      title: "Prøductïv",
+      repoLink: 'https://github.com/gary-rivera/flappy-bird-vanilla-js',
+      deployedLink: 'https://gary-rivera.github.io/productiv/',
+      technologies: ['Javascript', 'React'],
+      img: productivImg,
+      gif: 'https://media.giphy.com/media/tz2gP43rtCZqyB0tHs/giphy.gif',
+      description: 'The project that cemented my passion for software engineering! A vanilla JS clone of the classic mobile game Flappy Bird. Left click the canvas to have the bird sprite navigate between the pipes and try to beat your high score!'
     }, 
     {
       title: "FlappyJS",
       repoLink: 'https://github.com/gary-rivera/flappy-bird-vanilla-js',
       deployedLink: 'https://gary-rivera.github.io/flappy-bird-vanilla-js/',
       technologies: ['Javascript'],
-      img: flappy,
-      gif: flappyGif,
-      description: 'A vanilla JS clone of the classic mobile game Flappy Bird! Left click the canvas to have Flappy fly between the pipes and try to beat your high score!'
+      img: flappyImg,
+      gif: 'https://media.giphy.com/media/I73muSeVxEMx1OiVCo/giphy.gif',
+      description: 'The project that cemented my passion for software engineering! A vanilla JS clone of the classic mobile game Flappy Bird. Left click the canvas to have the bird sprite navigate between the pipes and try to beat your high score!'
     }, 
   ]
 
